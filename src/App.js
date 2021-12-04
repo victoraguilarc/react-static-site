de54dfbffdb29e0e950dfd89e06a58ae2bc36d23
Victor Aguilar C <vicobits@gmail.com>
2021-12-04 12:00:21 -0600

    chore: update index

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Devfest 2021 - GDG La Paz
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gdg.community.dev/gdg-la-paz/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Entrar
         </a>
       </header>
     </div>
